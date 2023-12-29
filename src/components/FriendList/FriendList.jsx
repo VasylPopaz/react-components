028eb8f45c23b982d0243ee5704bf5ee8efdb123
VasylPopaz <pafos1997@gmail.com>
2023-12-29 17:12:13 +0100

    updated

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,7 +1,6 @@
 import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 import css from './FriendList.module.css';
 export const FriendList = ({ friends }) => {
-  console.log(friends);
   return (
     <ul className={css.list}>
       {friends.map(elem => (
